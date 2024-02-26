f67b538042649b3e893361a388e1ca5d3c7b486d
Tulili <tulilirockz@outlook.com>
2024-02-26 00:41:52 -0300

    fix(clean): a LOT faster cleanup code + no progress bar

--- cmd/layer/clean/clean.go
+++ cmd/layer/clean/clean.go
@@ -6,13 +6,10 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"slices"
+	"sync"
 
-	"github.com/jedib0t/go-pretty/v6/progress"
 	"github.com/spf13/cobra"
 	"github.com/ublue-os/bext/internal"
-	"github.com/ublue-os/bext/pkg/logging"
-	"github.com/ublue-os/bext/pkg/percentmanager"
 )
 
 var CleanCmd = &cobra.Command{
@@ -32,28 +29,6 @@ func init() {
 	fDryRun = CleanCmd.Flags().Bool("dry-run", false, "Do not actually clean anything, just print what would be deleted")
 }
 
-func getWhatNotToClean(clean []string) ([]string, error) {
-	var do_not_clean []string
-	for _, cleanthing := range clean {
-		fstat, err := os.Lstat(cleanthing)
-		if err != nil {
-			return nil, err
-		}
-		if fstat.Mode().Type() == os.ModeSymlink && fstat.Name() == internal.CurrentBlobName {
-			eval_link, err := filepath.EvalSymlinks(cleanthing)
-			if err != nil && !errors.Is(err, os.ErrNotExist) {
-				return nil, err
-			} else if errors.Is(err, os.ErrNotExist) {
-				continue
-			}
-			do_not_clean = append(do_not_clean, eval_link)
-			do_not_clean = append(do_not_clean, cleanthing)
-			break
-		}
-	}
-	return do_not_clean, nil
-}
-
 func cleanCmd(cmd *cobra.Command, args []string) error {
 	cache_dir, err := filepath.Abs(internal.Config.CacheDir)
 	if err != nil {
@@ -64,14 +39,13 @@ func cleanCmd(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	base_message := "Cleaning "
-	pw := percent.NewProgressWriter()
+	var wg sync.WaitGroup
 
-	var expectedSections int
 	for _, entry := range target_cache {
 		if !entry.IsDir() {
 			continue
 		}
+		slog.Info("Cleaning layer " + entry.Name())
 
 		entry_dir_path := path.Join(cache_dir, entry.Name())
 		entry_dir, err := os.ReadDir(entry_dir_path)
@@ -79,101 +53,61 @@ func cleanCmd(cmd *cobra.Command, args []string) error {
 			return err
 		}
 
-		var clean []string
-
-		for _, cache_blob := range entry_dir {
-			if cache_blob.IsDir() {
-				continue
-			}
-
-			clean = append(clean, path.Join(entry_dir_path, cache_blob.Name()))
-		}
-
-		do_not_clean, err := getWhatNotToClean(clean)
-		if err != nil {
-			return err
-		}
-
 		if len(entry_dir) < 1 {
-			expectedSections++
+			wg.Add(1)
+			go func() {
+				wg.Done()
+				os.Remove(entry_dir_path)
+			}()
 			continue
 		}
 
-		for _, cache_blob := range entry_dir {
-			if cache_blob.IsDir() {
-				continue
-			}
-			expectedSections++
-		}
-		expectedSections = expectedSections - len(do_not_clean)
-	}
-
-	delete_tracker := percent.NewIncrementTracker(&progress.Tracker{Message: base_message, Total: int64(100), Units: progress.UnitsDefault}, expectedSections)
-	pw.AppendTracker(delete_tracker.Tracker)
-
-	if !*internal.Config.NoProgress {
-		go pw.Render()
-		slog.SetDefault(logging.NewMuteLogger())
-	}
-
-	for _, entry := range target_cache {
-		if !entry.IsDir() {
-			continue
-		}
-		logmessage := base_message + entry.Name()
-		delete_tracker.Tracker.Message = logmessage
-		slog.Info(logmessage)
-
-		entry_dir_path := path.Join(cache_dir, entry.Name())
-		entry_dir, err := os.ReadDir(entry_dir_path)
-		if err != nil {
-			delete_tracker.Tracker.MarkAsErrored()
-			return err
-		}
-
-		if len(entry_dir) < 1 {
-			delete_tracker.IncrementSection()
-			os.Remove(entry_dir_path)
-			continue
-		}
-
-		var clean []string
-
-		for _, cache_blob := range entry_dir {
-			if cache_blob.IsDir() {
-				continue
-			}
-
-			clean = append(clean, path.Join(entry_dir_path, cache_blob.Name()))
-		}
-
-		do_not_clean, err := getWhatNotToClean(clean)
-		if err != nil {
-			delete_tracker.Tracker.MarkAsErrored()
-			return err
-		}
+		var do_not_clean map[string]bool = make(map[string]bool)
 
 		for _, provided_path := range *fExclude {
 			managed_path, err := filepath.Abs(path.Clean(provided_path))
 			if err != nil {
 				return err
 			}
-			do_not_clean = append(do_not_clean, managed_path)
+			do_not_clean[managed_path] = true
 		}
 
-		for _, cleanthing := range clean {
-			slog.Debug("Clean -> " + cleanthing)
-			if slices.Contains(do_not_clean, cleanthing) || *fDryRun {
+		for _, cache_blob := range entry_dir {
+			if cache_blob.IsDir() {
 				continue
 			}
-			delete_tracker.IncrementSection()
-			if err := os.Remove(cleanthing); err != nil {
-				delete_tracker.Tracker.MarkAsErrored()
+
+			cleanpath := path.Join(entry_dir_path, cache_blob.Name())
+
+			fstat, err := os.Lstat(cleanpath)
+			if err != nil {
 				return err
 			}
+
+			if fstat.Mode().Type() == os.ModeSymlink && fstat.Name() == internal.CurrentBlobName {
+				eval_link, err := filepath.EvalSymlinks(cleanpath)
+				if err != nil && !errors.Is(err, os.ErrNotExist) {
+					return err
+				} else if errors.Is(err, os.ErrNotExist) {
+					continue
+				}
+				do_not_clean[eval_link] = true
+				do_not_clean[cleanpath] = true
+				continue
+			}
+			if _, exists := do_not_clean[cleanpath]; exists || *fDryRun {
+				continue
+			}
+
+			slog.Debug("Cleaned path", slog.String("path", cleanpath))
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				os.Remove(cleanpath)
+			}()
 		}
 	}
-	delete_tracker.Tracker.MarkAsDone()
+	wg.Wait()
 
 	return nil
 }
